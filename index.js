a1c104c87856de1931cb53993cb536c3339b3a74
shaun <ycorredius@gmail.com>
2020-02-16 18:03:59 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+ let wakeDog = function(dogName,dogBreed){
+ console.log(`Wake ${dogName} the ${dogBreed}`);
+ return `Wake ${dogName} the ${dogBreed}`;
+}
+let leashDog = function(dogName,dogBreed){
+    console.log(`Leash ${dogName}the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`;
+}
+let walkToPark = function(dogName,dogBreed){
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+}
+var throwFrisbee = function(dogName, dogBreed){
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+}
+var walkHome = function(dogName,dogBreed){
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`;
+}
+var unleashDog = function(dogName,dogBreed){
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`;
+}
+
+let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+
+function exerciseDog (dogName,dogBreed) {
+    return routine.map(element=> element(dogName,dogBreed));
+}
